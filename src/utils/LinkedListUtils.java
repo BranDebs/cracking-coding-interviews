326dfce3e6dfeb82cc0b5abe9a1efa3318ff3eb9
brandonyeoxg <brandonyeoxg@gmail.com>
2018-12-15 15:19:50 +0800

    Update print method of linkedlist util to be more general

--- src/utils/LinkedListUtils.java
+++ src/utils/LinkedListUtils.java
@@ -1,16 +1,31 @@
 package utils;
 
 public class LinkedListUtils {
-    public static void printList(SinglyNode<Integer> head) {
+    public static void printList(SinglyNode<?> head) {
         StringBuilder sb = new StringBuilder();
         if (head == null) {
             sb.append("The linked list is null");
             System.out.println(sb.toString());
             return;
         }
-        for (SinglyNode<Integer> itr = head; itr != null; itr = itr.next) {
+        for (SinglyNode<?> itr = head; itr != null; itr = itr.next) {
             sb.append(itr.element);
             sb.append(' ');
+        }
+        System.out.println(sb.toString());
+    }
+    public static void printListExact(SinglyNode<?> head, int count) {
+        StringBuilder sb = new StringBuilder();
+        if (head == null) {
+            sb.append("The linked list is null");
+            System.out.println(sb.toString());
+            return;
+        }
+        SinglyNode<?> itr = head;
+        for (int i = 0; i < count; i++) {
+            sb.append(itr.element);
+            sb.append(' ');
+            itr = itr.next;
         }
         System.out.println(sb.toString());
     }
