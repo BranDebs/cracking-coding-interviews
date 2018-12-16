166da0b5163a5063037b3a380783e62589f1e5c3
Deborah Low <deborahlow97@gmail.com>
2018-12-16 13:30:24 +0800

    change ch2qns7 to O(N) solution

--- src/ch2/qns7.java
+++ src/ch2/qns7.java
@@ -5,13 +5,40 @@ import utils.SinglyNode;
 public class qns7 {
 
     public static boolean isIntersecting(SinglyNode<Integer> head1, SinglyNode<Integer> head2) {
-        SinglyNode<Integer> pointer1, pointer2;
+        //make the linkedlist same size
+        int size1 = 0;
+        int size2 = 0;
+        int shorterSize;
+        SinglyNode<Integer> longerLinkedList, shorterLinkedList;
         for (SinglyNode<Integer> itr = head1; itr != null; itr = itr.next) {
-            for (SinglyNode<Integer> jtr = head2; jtr != null; jtr = jtr.next) {
-                if (itr == jtr) {
-                    return true;
-                }
+            size1++;
+        }
+        for (SinglyNode<Integer> itr = head2; itr != null; itr = itr.next) {
+            size2++;
+        }
+        int diff = Math.abs(size1-size2);
+        if(size1>size2){
+            longerLinkedList = head1;
+            shorterLinkedList = head2;
+            shorterSize = size2;
+        }
+        else{
+            longerLinkedList = head2;
+            shorterLinkedList = head1;
+            shorterSize = size1;
+        }
+        while (diff!=0){
+            longerLinkedList = longerLinkedList.next;
+            diff--;
+        }
+
+        while(shorterSize!=0){
+            if(longerLinkedList == shorterLinkedList){
+                return true;
             }
+            longerLinkedList = longerLinkedList.next;
+            shorterLinkedList = shorterLinkedList.next;
+            shorterSize--;
         }
         return false;
     }
@@ -25,19 +52,25 @@ public class qns7 {
         intersection = head1;
         head1 = new SinglyNode<Integer>(2, head1);
         head1 = new SinglyNode<Integer>(1, head1);
+        head1 = new SinglyNode<Integer>(7, head1);
 
-        head2 = new SinglyNode<Integer>(9, null);
-        head2 = new SinglyNode<Integer>(7, head2);
-        intersection.next = head2;
         head2 = new SinglyNode<Integer>(8, intersection);
         head2 = new SinglyNode<Integer>(6, head2);
         head2 = new SinglyNode<Integer>(7, head2);
         head2 = new SinglyNode<Integer>(8, head2);
         head2 = new SinglyNode<Integer>(9, head2);
+        head2 = new SinglyNode<Integer>(6, head2);
+        head2 = new SinglyNode<Integer>(7, head2);
+        head2 = new SinglyNode<Integer>(8, head2);
+        head2 = new SinglyNode<Integer>(9, head2);
+
         //intersection is at 3
 
         boolean intersecting = isIntersecting(head1, head2);
         System.out.println(intersecting);
+        System.out.println("Answer should be true.");
+
+        System.out.println("=========================== ");
 
         head1 = new SinglyNode<Integer>(1, null);
         head1 = new SinglyNode<Integer>(1, head1);
@@ -57,6 +90,8 @@ public class qns7 {
 
         intersecting = isIntersecting(head1, head2);
         System.out.println(intersecting);
+        System.out.println("Answer should be false.");
+
 
     }
 }
