2397a670cbeb39a10c0981b1e019a6ccdf1b8e2c
Deborah Low <deborahlow97@gmail.com>
2018-12-16 12:29:38 +0800

    Add ch2qns3

--- src/ch2/qns3.java
+++ src/ch2/qns3.java
@@ -1,4 +1,44 @@
 package ch2;
 
+import utils.LinkedListUtils;
+import utils.SinglyNode;
+
 public class qns3 {
+    //page 106
+    public static void deleteMiddle(SinglyNode<Integer> middle){
+
+        SinglyNode<Integer> nextNode = middle.next;
+        middle.element = nextNode.element;
+        middle.next = nextNode.next;
+    }
+    public static void run() {
+        SinglyNode<Integer> head, middle;
+        head = new SinglyNode<Integer>(6, null);
+        head = new SinglyNode<Integer>(5, head);
+        head = new SinglyNode<Integer>(4, head);
+        head = new SinglyNode<Integer>(3, head);
+        middle = head;
+        head = new SinglyNode<Integer>(2, head);
+        head = new SinglyNode<Integer>(1, head);
+        System.out.println("Before: ");
+        LinkedListUtils.printList(head);
+        deleteMiddle(middle);
+        System.out.println("After: ");
+        LinkedListUtils.printList(head);
+
+        System.out.println("=========================== ");
+
+        middle = null;
+        head = new SinglyNode<Integer>(1, null);
+        head = new SinglyNode<Integer>(1, head);
+        head = new SinglyNode<Integer>(1, head);
+        head = middle;
+        head = new SinglyNode<Integer>(1, head);
+        head = new SinglyNode<Integer>(1, head);
+        System.out.println("Before: ");
+        LinkedListUtils.printList(head);
+        deleteMiddle(middle);
+        System.out.println("After: ");
+        LinkedListUtils.printList(head);
+    }
 }
