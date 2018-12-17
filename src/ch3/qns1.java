9e997d00ef6b03d8523a8217ea7324362098f8f2
Deborah Low <deborahlow97@gmail.com>
2018-12-17 18:24:30 +0800

    Add more questions, time and space complexity for each ques

--- src/ch3/qns1.java
+++ src/ch3/qns1.java
@@ -4,9 +4,9 @@ package ch3;
  * Three in one:
  *  Describe how you could use a single array to implement three stacks.
  *
- *  Parition the single array into 3 separate regions, A B and C.
+ *  Partition the single array into 3 separate regions, A B and C.
  *  Note the start idx of each of the region. The stack on region A cannot grow past the start of region B.
- *  Stack on region B cannott grow past the start idx of region C.
+ *  Stack on region B cannot grow past the start idx of region C.
  */
 public class qns1 {
     public static void run() {
