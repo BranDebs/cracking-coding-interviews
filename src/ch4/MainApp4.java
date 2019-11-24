10560bf4d4354a802a2177f193de17ff5e3a2dd9
brandonyeoxg <brandonyeoxg@gmail.com>
2019-11-24 10:49:04 +0800

    feat(ch4): add solution for qns4_1

--- src/ch4/MainApp4.java
+++ src/ch4/MainApp4.java
@@ -2,6 +2,6 @@ package ch4;
 
 public class MainApp4 {
   public static void main(String[] args) {
-
+    qns4_1.tests();
   }
 }
--- src/ch4/qns4_1.java
+++ src/ch4/qns4_1.java
@@ -1,5 +1,48 @@
 package ch4;
 
+import utils.Graph.Digraph;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class qns4_1 {
-  
+  private static boolean RouteBetweenNodes(Digraph G, String src, String dst) {
+    if (G == null || src == null || dst == null) {
+      return false;
+    }
+
+    Deque<String> bfs = new LinkedList<>();
+    bfs.offerLast(src);
+    while (bfs.size() != 0) {
+      String curNode = bfs.poll();
+      if (curNode.equals(dst)) {
+        return true;
+      }
+      Iterable<String> adjNodes = G.adj(curNode);
+      if (adjNodes != null) {
+        for (String node : adjNodes) {
+          bfs.offerLast(node);
+        }
+      }
+    }
+    return false;
+  }
+
+  public static void tests() {
+    Digraph emptyG = new Digraph(0);
+    System.out.printf("Empty graph path %b\n", RouteBetweenNodes(emptyG, "A", "Z"));
+
+    Digraph notConnGraph = new Digraph(5);
+    notConnGraph.addEdge("A", "B");
+    notConnGraph.addEdge("A", "C");
+    notConnGraph.addEdge("Y", "Z");
+    System.out.printf("Not connected graph %b\n", RouteBetweenNodes(notConnGraph, "A", "Z"));
+
+    Digraph connGraph = new Digraph(5);
+    connGraph.addEdge("A", "B");
+    connGraph.addEdge("A", "C");
+    connGraph.addEdge("Y", "Z");
+    connGraph.addEdge("C", "Z");
+    System.out.printf("Not connected graph %b\n", RouteBetweenNodes(connGraph, "A", "Z"));
+  }
 }
