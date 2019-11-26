afb81eb8c3651545c0acef090f6e0a405d9c2325
brandonyeoxg <brandonyeoxg@gmail.com>
2019-11-26 22:05:29 +0800

    feat(ch6): add qns6_1

---
+++ src/ch6/qns6_1.java
@@ -0,0 +1,9 @@
+package ch6;
+
+public class qns6_1 {
+  // Order each bottle as 1, 2, ..., 20.
+  // For each numbered bottle, take out the pill associated to its assigned id.
+  // Bottle id 1 will take out 1 pill, bottle id n will be n pill.
+  // Based on 1 + 2 + .. + 20 = 20(21)/2 = 210 grams in total assuming all 1.0g
+  // (Final weight - 210) * 10 will give us the idea of the bottle id
+}
