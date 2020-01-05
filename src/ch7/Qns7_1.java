9b91a3c83f26f317c0e3a3580204238b037f81ce
brandonyeoxg <brandonyeoxg@gmail.com>
2020-01-05 20:00:23 +0800

    feat(Qns7_1): add Qns7_1

---
+++ src/ch7/Qns7_1.java
@@ -0,0 +1,29 @@
+package ch7;
+
+public class Qns7_1 {
+  /*
+    Card
+    * ID string
+    * Suit enum
+    * Val int
+
+    Deck
+    * CardCap int
+    * Cards []Card
+
+    CardGame intf
+    * Init()
+    * Deal()
+    * Update()
+    * End()
+
+    BackJack impl CardGame
+    * Players []Player
+    * PlayDeck Deck
+
+    Player
+    * Cards []Card
+    * Points int
+
+  */
+}
